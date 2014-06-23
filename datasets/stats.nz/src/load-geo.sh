3bd9442b64856af015bcc1f701193012728cb37b
Andrew Harvey <andrew.harvey@barbador.com>
2014-06-23 11:41:00 +1000

    statsnz load au and mb into psql

--- datasets/stats.nz/src/load-geo.sh
+++ datasets/stats.nz/src/load-geo.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-shp2pgsql extracted/ESRI\ shapefile\ Output/2013\ Digital\ Boundaries\ Generalised\ Full/AU2013_GV_Full.shp statsnz.au | psql
+shp2pgsql extracted/ESRI\ shapefile\ Output/2013\ Digital\ Boundaries\ Generalised\ Clipped/AU2013_GV_Clipped.shp statsnz.au | psql
+shp2pgsql extracted/ESRI\ shapefile\ Output/2013\ Digital\ Boundaries\ Generalised\ Clipped/MB2013_GV_Clipped.shp statsnz.mb | psql
+
